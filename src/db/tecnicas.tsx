03985015f034c98b95cf8c27b0a69e82885f377f
an4cbs <anadossantosux@gmail.com>
2024-10-31 19:52:13 -0300

    pensar em voz alta adicionado

--- src/db/tecnicas.tsx
+++ src/db/tecnicas.tsx
@@ -31,32 +31,41 @@ export const technics: TecnicaProps[] = [
     ],
     fullDescription: (
       <Typography variant="body1">
-        Avaliar a qualidade de interação do usuário com o sistema, medir
-        impactos, identificar desconfortos observando o uso do produto.
+        Pensar em voz alta, do inglês <i>think alound</i>, é uma técnica
+        utilizada para compreender o pensamento do usuário, onde ele verbaliza
+        suas ações e pensamentos enquanto utiliza o sistema.
         <br />
         <br />
-        Avaliar a qualidade de interação do usuário com o sistema, medir
-        impactos, identificar desconfortos..
+        A utilização desta técnica elimina a necessidade de interpretação pelo
+        avaliador do que se passa na mente do usuário, pois o usuário expressa
+        diretamente o que está pensando. Eliminar a necessidade de interpretação
+        pode ser interessante, evitando falhas na comunicação, especialmente
+        quando o avaliador não tem experiência suficiente em interpretar as
+        ações do usuário em avaliações de usabilidade.
         <br />
         <br />
-        Avaliar a qualidade de interação do usuário com o sistema, medir
-        impactos, identificar desconfortos observando o uso do produto.Avaliar a
-        qualidade de interação do usuário com o sistema, medir impactos,
-        identificar desconfortos observando o uso do produto.
+        É uma técnica considerada simples e útil, e pode ser facilmente aplicada
+        com métodos de observação, como teste de usabilidade por exemplo.
+        <br />
+        <br />
+        Por meio desta técnica pode-se obter dados qualitativos: dúvidas quanto
+        aos elementos da interface, dificuldades do participante, opiniões,
+        insights, e até mesmo sugestões de melhorias.
       </Typography>
     ),
+    exemples: [
+      "Ao realizar um teste de usabilidade, aquele onde o avaliador observa o participante realizando tarefas em um sistema, o avaliador pode solicitar ao participante antes do inicio da avaliação que faça uso da técnica, verbalizando o que pensa enquanto realiza as tarefas. Pensamentos do tipo 'Acho que esse botão me levará a tela de cadastro', 'Estou buscando pela seção de Configuração neste menu', são exemplos de pensamentos que podem ser expressos pelo participante.",
+    ],
+
     goodPractices: [
-      "Interaja o mínimo possível com o usuário.",
-      "Estas infromaçoes estrão em tópicos 2",
-      "Este é o topico 3",
+      "Uma boa prática é avisar o participante de que ele pode expressar os questionamentos que se passam na sua mente, mas que você ou o avaliador observador não responderão todas as dúvidas.",
+      "Recomenda-se responder apenas as dúvidas que não estejam relacionadas as dificuldades de usabilidade do sistema. Como por exemplo, uma dúvida causadas pelo baixo nível de fidelidade do sistema avaliado: o participante pode clicar em um botão que deveria leva-lo a alguma tela, no caso de ser um protótipo parcialmente navegável, o botão pode não realizar a ação esperada, e o participante pode perguntar o porquê. Neste caso, recomenda-se responder a dúvida, pois ela não está relacionada a usabilidade do sistema, mas sim a falta de fidelidade do protótipo.",
     ],
     attentionPoints: [
-      "Estas infromaçoes estrão em tópicos, esse é o topico 1",
-      "Estas infromaçoes estrão em tópicos, esse é o topico 1",
-      "Estas infromaçoes estrão em tópicos, esse é o topico 1",
-      "Estas infromaçoes estrão em tópicos, esse é o topico 1",
+      "Esta técnica depende muito do participante: alguns podem se distrair e parar de falar enquanto realizam alguma atividade. Outros podem gastar mais tempo e esforço relatando o que pensam do que executando as tarefas solicitadas.",
+      "Se não for aplicada de forma adequada, essa técnica pode interferir nos resultados da avaliação, como, por exemplo, no tempo de execução das tarefas, pois o participante pode gastar mais tempo pensando e falando e esse tempo acaba sendo contabilizado na avaliação.",
     ],
-    more: ["https://www.google.com"],
+    more: ["https://doi.org/10.26522/brocked.v12i2.38"],
   },
   {
     id: "gravacao_e_analise",
