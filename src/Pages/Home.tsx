16b2c1242b4649a25aadf43133eb29c65cab0c3e
an4cbs <anadossantosux@gmail.com>
2024-11-14 20:51:41 -0300

    Enhance HomePage with methods dropdown menu and tooltip descriptions for inspection, observation, and investigation methods

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,24 +2,59 @@ import {
   Box,
   Button,
   Chip,
+  Collapse,
+  ListItemText,
+  Menu as MuiMenu,
   Container,
+  Divider,
   IconButton,
+  MenuItem,
+  SvgIcon,
   TextField,
   Tooltip,
   Typography,
+  ListItemIcon,
 } from "@mui/material";
 import Card from "../Componentes/Card";
 import Footer from "../Componentes/Footer";
 import Header from "../Componentes/Header";
-import Menu from "../Componentes/Menu";
+import PageviewIcon from "@mui/icons-material/Pageview";
+
 import { MethodProps, methods } from "../db/methods";
-import { Delete, Search, Share } from "@mui/icons-material";
+import {
+  ArrowRight,
+  ContentPaste,
+  Delete,
+  QuestionAnswer,
+  Search,
+  Share,
+  Visibility,
+} from "@mui/icons-material";
 import { useMemo, useState } from "react";
 import { useGlobalContext } from "../GlobalProvider";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+import Menu from "../Componentes/Menu";
 
 export default function HomePage() {
+  const [openMethods, setOpenMethods] = useState(false);
   const [focused, setFocused] = useState(false);
   const [search, setSearch] = useState("");
+  const [anchorMethods, setAnchorMethods] = useState<HTMLButtonElement | null>(
+    null
+  );
+
+  const handleOpenMethodsMenu = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    setAnchorMethods(event.currentTarget);
+  };
+
+  const handleCloseMethodsMenu = () => {
+    setAnchorMethods(null);
+  };
+
+  const openMethodsMenu = Boolean(anchorMethods);
 
   const {
     handleScoreByMethod,
@@ -158,14 +193,20 @@ export default function HomePage() {
               flexGrow: 1,
             }}
           >
-            <Box>
+            <Box
+              sx={{
+                display: "flex",
+                // alignItems: "center",
+                justifyContent: "space-between",
+                position: "relative",
+              }}
+            >
               <TextField
                 sx={{
                   fieldset: {
                     border: "none",
                   },
                   borderRadius: 4,
-
                   backgroundColor: "#Ffff",
                 }}
                 onFocus={() => setFocused(true)}
@@ -203,7 +244,203 @@ export default function HomePage() {
                     }),
                 }}
               />
+              <Box
+                sx={{
+                  backgroundColor: "#E7EFF0",
+                  borderRadius: openMethods ? 2 : 5,
+                  padding: 2,
+                  maxWidth: 150,
+                  minWidth: 150,
+                  mr: -2,
+                  cursor: "pointer",
+                  position: "absolute",
+                  zIndex: 1,
+                  right: 16,
+                }}
+                onClick={() => setOpenMethods((prev) => !prev)}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    // alignItems: "center",
+                  }}
+                >
+                  <Typography sx={{ fontWeight: 500 }}>Métodos</Typography>
+                  {openMethods ? (
+                    <SvgIcon
+                      sx={{
+                        color: "#adadad",
+                      }}
+                      fontSize="small"
+                    >
+                      <IoIosArrowUp />
+                    </SvgIcon>
+                  ) : (
+                    <SvgIcon
+                      sx={{
+                        color: "#adadad",
+                      }}
+                      fontSize="small"
+                    >
+                      <IoIosArrowDown />
+                    </SvgIcon>
+                  )}
+                </Box>
+                {openMethods && (
+                  <Box
+                    sx={{
+                      paddingY: 1,
+                      display: "flex",
+                      flexDirection: "column",
+                      gap: 1,
+                      mt: 1,
+                    }}
+                  >
+                    <Box>Técnicas</Box>
+                    <Divider />
+                    <Box
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: 1,
+                        cursor: "pointer",
+                      }}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleOpenMethodsMenu(e);
+                      }}
+                    >
+                      Métodos <ArrowRight />
+                    </Box>
+                    <MuiMenu
+                      anchorEl={anchorMethods}
+                      open={openMethodsMenu}
+                      onClose={handleCloseMethodsMenu}
+                      anchorOrigin={{
+                        vertical: "top",
+                        horizontal: "right",
+                      }}
+
+                      // transformOrigin={{
+                      //   vertical: "top",
+                      //   horizontal: "center",
+                      // }}
+                    >
+                      <Tooltip
+                        title="Métodos de inspeção envolvem a interação do avaliador com o sistema, sem a participação direta do usuário. O objetivo é identificar antecipadamente problemas que os usuários possam encontrar, examinando detalhadamente a interface."
+                        placement="left"
+                        arrow
+                      >
+                        <MenuItem sx={{ gap: 1 }}>
+                          <Box
+                            sx={{
+                              borderRadius: 50,
+                              backgroundColor: "#86ADB5",
+                              width: 30,
+                              height: 30,
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center",
+                            }}
+                          >
+                            <PageviewIcon
+                              fontSize="small"
+                              sx={{
+                                color: "#ffff",
+                              }}
+                            />
+                          </Box>
+                          <Typography>Inspeção</Typography>
+                        </MenuItem>
+                      </Tooltip>
+                      <Divider />
+                      <Tooltip
+                        title="Métodos de observação envolvem mínima interação do avaliador com o usuário e o sistema. Os dados são obtidos ao observar o usuário interagindo com o sistema, permitindo identificar problemas reais."
+                        placement="left"
+                        arrow
+                      >
+                        <MenuItem sx={{ gap: 1 }}>
+                          <Box
+                            sx={{
+                              borderRadius: 50,
+                              backgroundColor: "#86ADB5",
+                              width: 30,
+                              height: 30,
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center",
+                            }}
+                          >
+                            <Visibility
+                              fontSize="small"
+                              sx={{
+                                color: "#ffff",
+                              }}
+                            />
+                          </Box>
+                          <Typography>Observação</Typography>
+                        </MenuItem>
+                      </Tooltip>
+                      <Divider />
+                      <Tooltip
+                        title="Métodos de investigação envolvem maior interação dialogada entre avaliador e usuário, permitindo acesso direto a suas opiniões, expectativas e comportamentos."
+                        placement="left"
+                        arrow
+                      >
+                        <MenuItem sx={{ gap: 1 }}>
+                          <Box
+                            sx={{
+                              borderRadius: 50,
+                              backgroundColor: "#86ADB5",
+                              width: 30,
+                              height: 30,
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center",
+                            }}
+                          >
+                            <QuestionAnswer
+                              fontSize="small"
+                              sx={{
+                                color: "#ffff",
+                              }}
+                            />
+                          </Box>
+                          <Typography>Investigação</Typography>
+                        </MenuItem>
+                      </Tooltip>
+                      <Divider />
+                      <MenuItem sx={{ gap: 1 }}>
+                        <Box
+                          sx={{
+                            borderRadius: 50,
+                            backgroundColor: "#86ADB5",
+                            width: 30,
+                            height: 30,
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                          }}
+                        >
+                          <Typography
+                            sx={{
+                              color: "#ffff",
+                              fontWeight: 650,
+                              fontSize: "1.2rem",
+                            }}
+                          >
+                            M
+                          </Typography>
+                        </Box>
+                        <Typography>Todos</Typography>
+                      </MenuItem>
+                    </MuiMenu>
+                  </Box>
+                )}
+              </Box>
             </Box>
+
             <Box>
               <Typography
                 sx={{ fontWeight: 600, fontSize: "1.5rem", marginBottom: -1.0 }}
